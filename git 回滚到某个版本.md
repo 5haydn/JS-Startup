fd20f5793b76f4c21fc14c289fcb12601ff0de1a
李朋 <haydn@LIs-iMac.local>
2017-09-02 00:52:27 +0800

    clean

--- git 回滚到某个版本.md
+++
@@ -1,8 +0,0 @@
-git reset A
-
-git clean -f
-
-
-
-> [git撤销commit的三个方法](http://www.awaimai.com/1336.html)
-
--- tmp.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import re as re
-import scipy
-
-%matplotlib inline
